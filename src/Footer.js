f0e016e996996f62823221b9cc737596117793ed
Quyet12345 <quyet.ph@isofhcare.com>
2020-06-04 08:53:44 +0700

    remove code

--- src/Footer.js
+++ src/Footer.js
@@ -6,52 +6,8 @@ export default class Footer extends Component {
       <footer className="page-footer" role="contentinfo">
         <div className="d-flex align-items-center flex-1 text-muted">
           <span className="hidden-md-down fw-700">
-            2019 © SmartAdmin by&nbsp;
-            <a
-              href="https://www.gotbootstrap.com"
-              className="text-primary fw-500"
-              title="gotbootstrap.com"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              gotbootstrap.com
-            </a>
+            Quản lý vật tư By Hữu Quyết
           </span>
-        </div>
-        <div>
-          <ul className="list-table m-0">
-            <li>
-              <a
-                href="intel_introduction.html"
-                className="text-secondary fw-700"
-              >
-                About
-              </a>
-            </li>
-            <li className="pl-3">
-              <a
-                href="info_app_licensing.html"
-                className="text-secondary fw-700"
-              >
-                License
-              </a>
-            </li>
-            <li className="pl-3">
-              <a href="info_app_docs.html" className="text-secondary fw-700">
-                Documentation
-              </a>
-            </li>
-            <li className="pl-3 fs-xl">
-              <a
-                href="https://wrapbootstrap.com/user/MyOrange"
-                className="text-secondary"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <i className="fal fa-question-circle" aria-hidden="true" />
-              </a>
-            </li>
-          </ul>
         </div>
       </footer>
     );
